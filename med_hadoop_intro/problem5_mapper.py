edeb33da106a1a6a2dcb84881240228d087127ac
Aaron Zhang <aaronz@live.unc.edu>
2018-04-12 10:45:35 -0400

    ready for topsail production

--- med_hadoop_intro/problem5_mapper.py
+++ med_hadoop_intro/problem5_mapper.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python
+import re
+import sys
+LOCAL_TEST_PATH = './test.txt'
+#Need Siteid, BackgroundID, Medication
+#def local():
+#    with open (LOCAL_TEST_PATH, "r") as f, open ("output5.txt","w") as of:
+#        for line in f:
+#            strippedLine = line.strip()
+#            if strippedLine!="":
+#                delimList = strippedLine.split(',')
+#                siteID = delimList[0]
+#                backgroundID = delimList[1]
+#                medication = delimList[5]
+#                outputMap(siteID,backgroundID,medication)
+#                of.write("%s,%s,%s\t1\n" % (siteID, backgroundID, medication))
+
+def outputMap(siteID, backgroundID, medication):
+    print "%s,%s,%s\t1" % (siteID, backgroundID, medication)
+
+def production():
+    for line in sys.stdin:
+        strippedLine = line.strip()
+        if strippedLine!="":
+            delimList = strippedLine.split(',')
+            siteID = delimList[0]
+            backgroundID = delimList[1]
+            medication = delimList[5]
+            if siteID!="siteId":
+                outputMap(siteID,backgroundID,medication)
+
+production()
--- med_hadoop_intro/problem5_reducer.py
+++ med_hadoop_intro/problem5_reducer.py
@@ -1,1 +1,89 @@
 #!/usr/bin/python
+import re
+import sys
+import operator
+
+def production():
+    (lastSiteID, lastBackgroundID, lastMedication) = (None, None, None)
+    medicationToOccurences = {}
+    for line in sys.stdin:
+        splitLine = line.strip().split("\t")
+        keyLine = splitLine[0].split(",")
+        val = splitLine[1]
+        siteID = keyLine[0]
+        backgroundID = keyLine[1]
+        medication = keyLine[2]
+        #print siteID, backgroundID, medication, val
+        #print lastSiteID, lastBackgroundID, lastMedication
+        #dict from medication to occurences, addition if unique siteID and backgroundID...
+        #on new siteID, find the dictionary key with highest value and output
+        if lastSiteID and lastBackgroundID and lastMedication:
+            #print lastSiteID, siteID
+            if lastSiteID!=siteID:
+                #New site
+                #find key with highest val
+                if len(medicationToOccurences)!=0:
+                    highestOccurence = max(medicationToOccurences.iteritems(), key=operator.itemgetter(1))[0]
+                    print lastSiteID,": ",highestOccurence, "Occurences: ", medicationToOccurences[highestOccurence]
+                #clear dictionary
+                medicationToOccurences.clear()
+                medicationToOccurences[medication] = 1
+            elif lastBackgroundID!=backgroundID or lastMedication!=medication:
+                if medication not in medicationToOccurences:
+                    #New entry
+                    #print "\tnew entry"
+                    medicationToOccurences[medication] = 1
+                else:
+                    #Add to existing entry
+                    #print "\texisting entry"
+                    occurences = medicationToOccurences[medication]
+                    medicationToOccurences[medication] = occurences+1
+        else:
+            medicationToOccurences[medication] = 1
+        (lastSiteID, lastBackgroundID, lastMedication) = (siteID, backgroundID, medication)
+    if len(medicationToOccurences)>0:
+        highestOccurence = max(medicationToOccurences.iteritems(), key=operator.itemgetter(1))[0]
+        print lastSiteID,": ",highestOccurence, "Occurences: ", medicationToOccurences[highestOccurence]
+#def local():
+#    filePath = "output5.txt"
+#    (lastSiteID, lastBackgroundID, lastMedication) = (None, None, None)
+#    medicationToOccurences = {}
+#    with open(filePath,"r") as f:
+#        for line in f:
+#            splitLine = line.strip().split("\t")
+#            keyLine = splitLine[0].split(",")
+#            val = splitLine[1]
+#            siteID = keyLine[0]
+#            backgroundID = keyLine[1]
+#            medication = keyLine[2]
+            #print siteID, backgroundID, medication, val
+            #print lastSiteID, lastBackgroundID, lastMedication
+            #dict from medication to occurences, addition if unique siteID and backgroundID...
+            #on new siteID, find the dictionary key with highest value and output
+#            if lastSiteID and lastBackgroundID and lastMedication:
+                #print "\t",lastSiteID, siteID
+#                if lastSiteID!=siteID:
+                    #New site
+                    #find key with highest val
+#                    if len(medicationToOccurences)!=0:
+#                        highestOccurence = max(medicationToOccurences.iteritems(), key=operator.itemgetter(1))[0]
+#                        print lastSiteID,": ",highestOccurence, "Occurences: ", medicationToOccurences[highestOccurence]
+#                        medicationToOccurences.clear()
+#                    medicationToOccurences[medication] = 1
+#                elif lastBackgroundID!=backgroundID or lastMedication!=medication:
+#                    if medication not in medicationToOccurences:
+                        #New entry
+                        #print "\tnew entry"
+#                        medicationToOccurences[medication] = 1
+#                    else:
+                        #Add to existing entry
+                        #print "\texisting entry"
+#                        occurences = medicationToOccurences[medication]
+#                        medicationToOccurences[medication] = occurences+1
+#            else:
+#                medicationToOccurences[medication] = 1
+#            (lastSiteID, lastBackgroundID, lastMedication) = (siteID, backgroundID, medication)
+#        if len(medicationToOccurences)>0:
+#            highestOccurence = max(medicationToOccurences.iteritems(), key=operator.itemgetter(1))[0]
+#            print lastSiteID,": ",highestOccurence, "Occurences: ", medicationToOccurences[highestOccurence]
+production()
